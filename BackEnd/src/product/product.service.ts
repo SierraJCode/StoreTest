5082b328ac95421e456816f1eb2e46dd1d738f68
Jesus Sierra <sierraja06@gmail.com>
2024-07-09 15:46:00 -0500

    Acyualizando service

--- BackEnd/src/product/product.service.ts
+++ BackEnd/src/product/product.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-// import { ProductSchema } from './schema/product.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Product } from './interface/product.interface';
@@ -10,28 +9,53 @@ export class ProductService {
 
     constructor(@InjectModel('Product') private readonly productModel: Model<Product>){}
 
-    async findAll(): Promise<Product[]>{
+    async findAll():Promise<Product[]>{
         return this.productModel.find().exec();
     }
+
     async findOne(id: string): Promise<Product>{
         return this.productModel.findById(id).exec();
     }
-    
+
     async create(createProductDTO: CreateProductDTO): Promise<Product>{
-        const newProduct = new this.productModel(createProductDTO);
-        return newProduct.save()
+        return new this.productModel(createProductDTO).save();
     }
 
-    async update(id: string, createProductDTO:CreateProductDTO): Promise<Product>{
-        if(this.productModel.findById(id).exists){
-            return this.productModel.findByIdAndUpdate(id, createProductDTO, { new: true }).exec();
-        }else{
-            console.log('Producto no encontrado');
-        }
+    async update(id: string, createProductDTO: CreateProductDTO): Promise<Product>{
+        return this.productModel.findByIdAndUpdate(id, createProductDTO, { new: true } ).exec();
     }
 
-    async delete(id: string):Promise<Product>{
-        return this.productModel.findByIdAndDelete(id).exec();
+    async delete(id: string): Promise<Product>{
+        return this.productModel.findByIdAndUpdate(id)
     }
+    
+    
 }
+
+
+// constructor(@InjectModel('Product') private readonly productModel: Model<Product>){}
+
+// async findAll(): Promise<Product[]>{
+//     return this.productModel.find().exec();
+// }
+// async findOne(id: string): Promise<Product>{
+//     return this.productModel.findById(id).exec();
+// }
+
+// async create(createProductDTO: CreateProductDTO): Promise<Product>{
+//     const newProduct = new this.productModel(createProductDTO);
+//     return newProduct.save()
+// }
+
+// async update(id: string, createProductDTO:CreateProductDTO): Promise<Product>{
+//     if(this.productModel.findById(id).exists){
+//         return this.productModel.findByIdAndUpdate(id, createProductDTO, { new: true }).exec();
+//     }else{
+//         console.log('Producto no encontrado');
+//     }
+// }
+
+// async delete(id: string):Promise<Product>{
+//     return this.productModel.findByIdAndDelete(id).exec();
+// }
 
