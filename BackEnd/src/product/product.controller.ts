7d8540a0b69acfcb4d914cd5ebc62c144ecf628a
Jesus Sierra <sierraja06@gmail.com>
2024-07-09 15:58:31 -0500

    ProductController listo

--- BackEnd/src/product/product.controller.ts
+++ BackEnd/src/product/product.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDTO } from './dto/product.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+
+    constructor(private readonly productService: ProductService){}
+
+    @Get()
+    async findAll(){
+        return this.productService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string){
+        return this.productService.findOne(id);
+    }
+
+    @Post()
+    async create(@Body() createProductDTO: CreateProductDTO){
+        return this.productService.create(createProductDTO);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, createProductDTO: CreateProductDTO){
+        return this.productService.update(id, createProductDTO);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string){
+        return this.productService.delete(id);
+    }
+}
