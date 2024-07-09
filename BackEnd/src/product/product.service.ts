8b79f2e1adbee2cd51fed2ee9b313d903570fe8b
Jesus Sierra <sierraja06@gmail.com>
2024-07-09 09:33:38 -0500

    Se crea el metodo FindAll & FindOne en el service del product

--- BackEnd/src/product/product.service.ts
+++ BackEnd/src/product/product.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ProductSchema } from './schema/product.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product } from './interface/product.interface';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+
+    constructor(@InjectModel('Product') private readonly productModel: Model<Product>){}
+
+    async findAll(): Promise<Product[]>{
+        return this.productModel.find().exec();
+    }
+    async findOne(id: string): Promise<Product>{
+        return this.productModel.findById(id).exec();
+    }
+    
+}
