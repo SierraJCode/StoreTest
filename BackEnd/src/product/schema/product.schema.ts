fc7c371eb56da4cc1128849f846936e0ebbac529
Jesus Sierra <sierraja06@gmail.com>
2024-07-09 07:15:34 -0500

    Creacion del modelo product y su schema

---
+++ BackEnd/src/product/schema/product.schema.ts
@@ -0,0 +1,8 @@
+import { Schema } from "mongoose";
+
+export const ProductSchema = new Schema({
+    name: {type: String, require: true},
+    description: {type: String, require: true},
+    price: {type: String, require: true},
+    createAt: {type: String, require: true}
+})
