fcea2ece969a793c50b90d9ddbc6cc04ffcf388b
Jesus Sierra <sierraja06@gmail.com>
2024-07-09 19:46:18 -0500

    Añadiendo validaciones en el BackEnd

--- BackEnd/src/product/dto/product.dto.ts
+++ BackEnd/src/product/dto/product.dto.ts
@@ -1,6 +1,20 @@
+import { IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 export class CreateProductDTO {
+
+    @IsString()
+    @IsNotEmpty()
     readonly name: string;
+
+    @IsString()
+    @IsNotEmpty()
     readonly description: string;
+
+    @IsNumber()
+    @IsNotEmpty()
     readonly price: number;
+
+    @IsDate()
+    @IsNotEmpty()
     readonly createAt: Date;
 }
