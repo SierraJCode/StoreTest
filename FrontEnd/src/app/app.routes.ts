3e0e46586db05bbfa3e677edc647d02f00bad1b5
Jesus Sierra <sierraja06@gmail.com>
2024-07-10 13:02:41 -0500

    Probando ruta principal

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ProductComponent
+    },
+    {
+        path: 'product',
+        component: ProductComponent
+    }
+];
