ffc98c0dddcd878d606ef8ce0f56819ac66e6be8
Jesus Sierra <sierraja06@gmail.com>
2024-07-11 09:07:26 -0500

    Se crea el componente app-product y se hace un formulario de prueba

---
+++ FrontEnd/src/app/product/product.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../interface/product.interface';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterOutlet],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  
+  products:Product[] = []
+  
+  constructor(private productService: ProductService){}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    return this.productService.getProducts().subscribe(
+      datos => {
+        this.products = datos;
+    })
+  }
+}
