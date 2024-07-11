0fbbfdb53a8071f2a85bb99d1ba520befe343874
Jesus Sierra <sierraja06@gmail.com>
2024-07-11 09:05:19 -0500

    Definiendo nuevas ritas (editar y crear)

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { ProductComponent } from './product/product.component';
+import { FormProductComponent } from './form-product/form-product.component';
 
 export const routes: Routes = [
     {
@@ -9,5 +10,13 @@ export const routes: Routes = [
     {
         path: 'product',
         component: ProductComponent
+    },
+    {
+        path: 'product/create',
+        component: FormProductComponent
+    },
+    {
+        path: 'product/create/:id',
+        component: FormProductComponent
     }
 ];
