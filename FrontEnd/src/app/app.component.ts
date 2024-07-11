be167a92e616b370d5cdde988edf03b6d994ecd4
Jesus Sierra <sierraja06@gmail.com>
2024-07-11 09:04:57 -0500

    Add Moduls RouterOulet in AppComponent

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductService } from './services/product.service';
 import { ProductComponent } from "./product/product.component";
+import { FormProductComponent } from "./form-product/form-product.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductComponent],
+  imports: [RouterOutlet, ProductComponent, FormProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
