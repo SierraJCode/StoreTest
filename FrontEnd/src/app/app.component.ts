43d81be3fffad9410cc6791f406c9dc3f3503094
Jesus Sierra <sierraja06@gmail.com>
2024-07-12 09:47:43 -0500

    Desinstalando toastr

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -3,13 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { ProductService } from './services/product.service';
 import { ProductComponent } from "./product/product.component";
 import { FormProductComponent } from "./form-product/form-product.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductComponent, FormProductComponent, BrowserAnimationsModule, ToastrModule],
+  imports: [RouterOutlet, ProductComponent, FormProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
